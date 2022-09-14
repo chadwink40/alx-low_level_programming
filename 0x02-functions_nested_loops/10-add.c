fb68b327ec80047cfc4f59f0c0fdfb07ef2f3966
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:32:07 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h> 
+#include <stdio.h>
+
+/**
+ * add - addition operation
+ * @i: The number to be added.
+ * @j: The number to be added.
+ * Return: the sum of two numbers.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}

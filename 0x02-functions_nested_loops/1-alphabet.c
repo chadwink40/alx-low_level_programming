51d2dfbc79e14b08655d7699f29965303452bd89
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:18:47 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "alphabet.h"
+
+/**
+ * print_alphabet - from a-z.
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

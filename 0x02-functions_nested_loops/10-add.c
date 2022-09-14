5e4524c5423a0e81c3227fea171ef6042c78629a
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 12:15:11 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - addition operation
+ * @i: The number to be added.
+ * @j: The number to be added.
+ * Return: the sum of two numbers.
  */
-
-
 int add(int i, int j)
 {
 	return (i + j);

632267742efd4b79e9a2060090c55270a0b31f22
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:17:47 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - prints the string "alx"
+  *
+  * Return: 0 on success.
+  */
+
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "Holberton";
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

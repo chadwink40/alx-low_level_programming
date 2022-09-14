87479ebd7dd49977fdf0448058183f2c218fdddb
chadwin40 <lolademyschool@gmail.com>
2022-09-14 02:40:08 -0700

    kraus

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include <stdlib.h> 
 #include <stdio.h>
 
-/**
+/**main
  * add - addition operation
  * @i: The number to be added.
  * @j: The number to be added.
  * Return: the sum of two numbers.
  */
+
 int add(int i, int j)
 {
 	return (i + j);

94cb7d36beb7465b8330f87b52778efab777655d
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:24:52 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _isalpha - check whether a character is alphabet or not.
+ * @c: character to be checked.
+ * Return: 1 if the character is an alphabet.
+ * Return 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}

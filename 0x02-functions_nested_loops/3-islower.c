03bc423a582e0ae465295b46c3a7b7a77f7b59ab
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:23:58 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _islower - tell a character is a lowercase
+ * @c: character to be input.
+ * Return: 1 if it is a lowercase, 0 is not a lowercase.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

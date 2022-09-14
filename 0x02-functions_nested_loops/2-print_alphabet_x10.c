0ad0696dba345e47ab792f4b6dd91e2b3513e751
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-14 10:19:53 +0100

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "blonc.h"
+
+/**
+ * print_alphabet_x10 - print_alphabet 10 times.
+ * Return: Void.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}

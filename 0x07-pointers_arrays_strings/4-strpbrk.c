de843c9f048d3c0ee73cfcadccabd818e805416a
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-26 22:10:48 +0100

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}

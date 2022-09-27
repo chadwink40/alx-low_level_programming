db2ad7a65019ff89fbef82845115e34150e9af15
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-09-27 22:48:53 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,22 +1,44 @@
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ * _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
+
 unsigned int _strspn(char *s, char *accept)
+
 {
-	unsigned int i, j;
+	int i, j;
+	int count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
 		j = 0;
-		while (accept[j] != '\0' && s[i] != accept[j])
-			j++;
-		if (accept[j] == '\0')
-			return (i);
-		i++;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		{
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
+		}
+
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
 	}
-	return (i);
+
+	return (count); /*return the value of count*/
 }

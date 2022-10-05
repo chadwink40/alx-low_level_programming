a432e1bf576b8a41dce36497ad80a9ece9043042
Officialstakeholder <34741592+chadwink40@users.noreply.github.com>
2022-10-05 06:52:56 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the logo of the program
+ * @argc: Count the args
+ * @argv: Args
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
